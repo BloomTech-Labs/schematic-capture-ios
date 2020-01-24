4d8da686839fb93724fb4b25864d9a19fbbb45d3
Gi Pyo Kim <gipyok@gmail.com>
2020-01-23 21:21:33 -0800

    Created the project and added Firebase SDKs

---
+++ Schematic Capture/Schematic Capture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schematic Capture
+//
+//  Created by Gi Pyo Kim on 1/23/20.
+//  Copyright © 2020 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
