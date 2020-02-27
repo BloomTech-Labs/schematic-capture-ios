a16affaf0d4a53067b44d9bd0dd299655baef881
Gi Pyo Kim <gipyok@gmail.com>
2020-02-27 10:42:04 -0800

    Authentication unit test setup

--- Schematic Capture/Schematic CaptureTests/Schematic_CaptureTests.swift
+++ Schematic Capture/Schematic CaptureTests/Schematic_CaptureTests.swift
@@ -11,18 +11,40 @@ import Firebase
 import FirebaseStorage
 @testable import Schematic_Capture
 
+struct AsyncOperation<Value> {
+    let queue: DispatchQueue = .main
+    let closure: () -> Value
+
+    func perform(then handler: @escaping (Value) -> Void) {
+        queue.async {
+            let value = self.closure()
+            handler(value)
+        }
+    }
+}
+
 class Schematic_CaptureTests: XCTestCase {
         
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let loginController = LogInController()
         let user = User(email: "bob_johnson@lambdaschool.com", password: "testing123!")
+        let expectation = self.expectation(description: "loginController")
         
         loginController.logIn(with: user) { (error) in
             if let error = error {
                 XCTFail("\(error)")
+                return
             }
+            if loginController.bearer == nil {
+                XCTFail("No bearer")
+                return
+            }
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 15, handler: nil)
+        XCTAssertNotNil(loginController.bearer)
     }
     
     override func tearDown() {
@@ -41,58 +63,58 @@ class Schematic_CaptureTests: XCTestCase {
         }
     }
     
-//    func testDownloadSchematicsPDF() {
-//        let listAll = expectation(description: "List All")
-//        let getData = expectation(description: "Get Data")
-//
-//        let maxSize: Int64 = 1073741824 // 1GB
-//        let storage = Storage.storage()
-//        let storageRef = storage.reference()
-//        var pdfRef: String?
-//        let schematicRef = storageRef.child("1")
-//            .child("1")
-//            .child("1")
-//            .child("1")
-//        schematicRef.listAll { (listResult, error) in
-//            if let error = error {
-//                XCTFail("\(error)")
-//                return
-//            }
-//            let listFullPaths = listResult.items.map { $0.fullPath }
-//            for path in listFullPaths {
-//                if path.contains(".PDF") || path.contains(".pdf") {
-//                    pdfRef = path
-//                    break
-//                }
-//            }
-//            guard let pdfRef = pdfRef else {
-//                XCTFail("No PDF file found in \(schematicRef.fullPath)")
-//                return
-//            }
-//            let start = pdfRef.lastIndex(of: "/")!
-//            let newStart = pdfRef.index(after: start)
-//            let range = newStart..<pdfRef.endIndex
-//            let pdfNameString = String(pdfRef[range])
-//            print(pdfNameString)
-//            schematicRef.child(pdfNameString).getData(maxSize: maxSize) { (data, error) in
-//                if let error = error {
-//                    XCTFail("\(error)")
-//                    return
-//                }
-//
-//                guard let data = data else {
-//                    XCTFail("No schematic pdf returned")
-//                    return
-//                }
-//                print("\(data)")
-//                getData.fulfill()
-//            }
-//            listAll.fulfill()
-//        }
-//        waitForExpectations(timeout: 10) { (error) in
-//            if let error = error {
-//                XCTFail("\(error)")
-//            }
-//        }
-//    }
+    func testDownloadSchematicsPDF() {
+        let listAll = expectation(description: "List All")
+        let getData = expectation(description: "Get Data")
+
+        let maxSize: Int64 = 1073741824 // 1GB
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        var pdfRef: String?
+        let schematicRef = storageRef.child("1")
+            .child("1")
+            .child("1")
+            .child("1")
+        schematicRef.listAll { (listResult, error) in
+            if let error = error {
+                XCTFail("\(error)")
+                return
+            }
+            let listFullPaths = listResult.items.map { $0.fullPath }
+            for path in listFullPaths {
+                if path.contains(".PDF") || path.contains(".pdf") {
+                    pdfRef = path
+                    break
+                }
+            }
+            guard let pdfRef = pdfRef else {
+                XCTFail("No PDF file found in \(schematicRef.fullPath)")
+                return
+            }
+            let start = pdfRef.lastIndex(of: "/")!
+            let newStart = pdfRef.index(after: start)
+            let range = newStart..<pdfRef.endIndex
+            let pdfNameString = String(pdfRef[range])
+            print(pdfNameString)
+            schematicRef.child(pdfNameString).getData(maxSize: maxSize) { (data, error) in
+                if let error = error {
+                    XCTFail("\(error)")
+                    return
+                }
+
+                guard let data = data else {
+                    XCTFail("No schematic pdf returned")
+                    return
+                }
+                print("\(data)")
+                getData.fulfill()
+            }
+            listAll.fulfill()
+        }
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("\(error)")
+            }
+        }
+    }
 }
