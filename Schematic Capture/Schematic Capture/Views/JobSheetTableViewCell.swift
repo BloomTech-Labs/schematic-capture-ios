6d08654ba9b833f6e488388e7463efa9c8d6c9ee
Troy Christian <apostasy8@gmail.com>
2020-04-10 01:27:17 -0400

    Jobsheet can be toggled to complete/incomplete in JobsTableViewController

--- Schematic Capture/Schematic Capture/Views/JobSheetTableViewCell.swift
+++ Schematic Capture/Schematic Capture/Views/JobSheetTableViewCell.swift
@@ -12,7 +12,25 @@ class JobSheetTableViewCell: UITableViewCell {
 
     @IBOutlet weak var jobSheetNameLabel: UILabel!
     @IBOutlet weak var numOfComponentsLabel: UILabel!
-    @IBOutlet weak var statusLabel: UILabel!
+//    @IBOutlet weak var statusLabel: UILabel!
+    
+    
+    @IBOutlet weak var statusButton: UIButton!
+      
+    @IBAction func statusToggleTapped(_ sender: Any) {
+        statusButton.isSelected.toggle()
+        
+        if statusButton.isSelected {
+            jobSheet?.status = "complete"
+        }
+        
+        if !statusButton.isSelected {
+            jobSheet?.status = "incomplete"
+        }
+        
+        print("The jobsheet is \(jobSheet?.status)")
+        
+    }
     
     var jobSheet: JobSheet? {
         didSet{
@@ -25,7 +43,7 @@ class JobSheetTableViewCell: UITableViewCell {
         
         jobSheetNameLabel.text = jobSheet.name
         numOfComponentsLabel.text = jobSheet.components != nil ? "\(jobSheet.components!.count) Components" : "0 Components"
-        statusLabel.text = jobSheet.status
+//        statusLabel.text = jobSheet.status
     }
 
 }
