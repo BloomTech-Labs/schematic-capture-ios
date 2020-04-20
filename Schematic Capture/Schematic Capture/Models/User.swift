55f4de3aac602eed0edfd61fd67f13e5a8a10f02
Troy Christian <apostasy8@gmail.com>
2020-04-20 17:29:50 -0400

    Another exclude pattern

--- Schematic Capture/Schematic Capture/Models/User.swift
+++ Schematic Capture/Schematic Capture/Models/User.swift
@@ -7,11 +7,7 @@
 //
 
 // Add question:String attribute to User
-//:comment for excluding Third party API from Code Climate
-//Attempt to exclude pods.
-//exclude all pods
-//try again
-//once more
+
 import Foundation
 
 struct Organization: Codable {
