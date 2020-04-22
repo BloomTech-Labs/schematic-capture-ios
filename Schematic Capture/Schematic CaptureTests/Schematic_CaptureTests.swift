e431e8b365b85bab64eb0c6b3104c3d1d184ed94
Troy Christian <apostasy8@gmail.com>
2020-04-22 13:49:36 -0400

    Add testDownLoadSchematics

--- Schematic Capture/Schematic CaptureTests/Schematic_CaptureTests.swift
+++ Schematic Capture/Schematic CaptureTests/Schematic_CaptureTests.swift
@@ -30,15 +30,19 @@ class Schematic_CaptureTests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let user = User(password: "Testing123!", username: "bob_johnson@lambdaschool.com")
-        let bearer = Bearer?.self
+        let bearer = Bearer(token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRpRFRmZU5GMUtjRWtXOTdnUExJcEc4NWl1YjIiLCJwYXNzd29yZCI6MSwiZW1haWwiOiJib2Jfam9obnNvbkBsYW1iZGFzY2hvb2wuY29tIiwicm9sZUlkIjoiVGVzdGluZzEyMyEiLCJpYXQiOjE1ODc1NzY2ODIsImV4cCI6MTU4NzY2MzA4Mn0.Ab_OLV463UTVY4gDvwTZt3orBOmBYGgEKPtDEWRVdUA")
+   
         let expectation = self.expectation(description: "loginController")
         
+        
+        //MARK: Test LoginController
         loginController.logIn(username: (user.username)!, password: user.password!, completion:  { (error) in
             if let error = error {
                 XCTFail("\(error)")
                 return
             }
             else {
+                
                 expectation.fulfill()
             }
             
@@ -75,4 +79,43 @@ class Schematic_CaptureTests: XCTestCase {
         wait(for:[expectation], timeout:10)
         
     }
+    
+  
+    
+    //MARK:Test ProjectController
+    
+    func testDownloadSchematics(){
+        let expectation = self.expectation(description: "download schematics executing without error")
+        projectController.bearer = Bearer(token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRpRFRmZU5GMUtjRWtXOTdnUExJcEc4NWl1YjIiLCJwYXNzd29yZCI6MSwiZW1haWwiOiJib2Jfam9obnNvbkBsYW1iZGFzY2hvb2wuY29tIiwicm9sZUlkIjoiVGVzdGluZzEyMyEiLCJpYXQiOjE1ODc1NzY2ODIsImV4cCI6MTU4NzY2MzA4Mn0.Ab_OLV463UTVY4gDvwTZt3orBOmBYGgEKPtDEWRVdUA")
+        
+        projectController.downloadSchematics(completion: { error in
+            let bearer = self.projectController.bearer
+            
+            if let bearer = bearer  {
+               expectation.fulfill()
+                return
+            }
+            else {
+                XCTFail()
+            
+           
+                
+            }
+            
+             
+            } )
+        wait(for:[expectation], timeout:10)
+    }
+       
+    
+    
+    
+    
+    func testDownloadAssignedJobs() {
+        let expectation = self.expectation(description: "Do")
+        projectController.downloadAssignedJobs()
+        
+        
+    }
+    
 }
