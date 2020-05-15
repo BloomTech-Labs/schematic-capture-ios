e7444ebee0d30bf8ac3bfd322fa827476d00d8c9
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-05-15 19:03:59 +0200

    Added UIView+String

---
+++ Schematic Capture/Schematic Capture/Extension/UIView+String.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+String.swift
+//  Schematic Capture
+//
+//  Created by Ufuk Türközü on 15.05.20.
+//  Copyright © 2020 GIPGIP Studio. All rights reserved.
+//
+
+import Foundation
--- Schematic Capture/Schematic Capture/View Controllers/ProjectsTableViewController.swift
+++ Schematic Capture/Schematic Capture/View Controllers/ProjectsTableViewController.swift
@@ -84,6 +84,8 @@ class ProjectsTableViewController: UITableViewController {
     
     func setupViews() {
         view.backgroundColor = .systemBackground
+        self.title = "Projects"
+        
     }
     
 }
@@ -136,3 +138,106 @@ extension ProjectsTableViewController: NSFetchedResultsControllerDelegate {
         }
     }
 }
+
+/*
+ import UIKit
+ import WebKit
+ import SwiftyDropbox
+ 
+ class HomeViewController: UIViewController, WKUIDelegate {
+ 
+ // MARK: - UI Elements
+ 
+ @IBOutlet weak var downloadProjectsButton: UIButton!
+ @IBOutlet weak var viewProjectsButton: UIButton!
+ @IBOutlet weak var uploadJobSheetsButton: UIButton! // not implemented
+ 
+ // MARK: - Properties
+ 
+ var loginController: AuthorizationController?
+ var projectController = ProjectController()
+ var webView: WKWebView!
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+ webView.uiDelegate = self
+ if let url = Bundle.main.url(forResource: "Pulse-1s-200px", withExtension: "svg") {
+ webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+ }
+ }
+ 
+ @IBAction func uploadJobSheets(_ sender: Any) {
+ DropboxClientsManager.authorizeFromController(UIApplication.shared,
+ controller: self,
+ openURL: {(url:URL) -> Void in UIApplication.shared.openURL(url)})
+ }
+ 
+ @IBAction func downloadSchematics(_ sender: Any) {
+ startLoadingScreen()
+ 
+ projectController.downloadAssignedJobs { (error) in
+ if let error = error {
+ self.stopLoadingScreen()
+ DispatchQueue.main.async {
+ // TODO: - Show Alert: "Unable to download assigned jobs", subTitle: "\(error)")
+ }
+ return
+ }
+ 
+ // adding these to stop download animation
+ self.stopLoadingScreen()
+ DispatchQueue.main.async {
+ // TODO: - Show Alert
+ }
+ }
+ }
+ 
+ @IBAction func signOut(_ sender: Any) {
+ guard let loginController = loginController else { return }
+ 
+ //        loginController.signOut { (error) in
+ //            if let error = error {
+ //                print("\(error)")
+ //                return
+ //            }
+ //
+ //            DispatchQueue.main.async {
+ //                if self.presentingViewController != nil {
+ //                    self.dismiss(animated: false, completion: {
+ //                        self.navigationController!.popToRootViewController(animated: true)
+ //                    })
+ //                } else {
+ //                    self.navigationController!.popToRootViewController(animated: true)
+ //                }
+ //            }
+ //        }
+ }
+ 
+ func startLoadingScreen() {
+ guard let webView = webView else { return }
+ 
+ DispatchQueue.main.async {
+ webView.translatesAutoresizingMaskIntoConstraints = false
+ webView.backgroundColor = .clear
+ webView.isOpaque = false
+ self.view.addSubview(webView)
+ 
+ webView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+ webView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+ webView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+ webView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -15).isActive = true
+ }
+ }
+ 
+ func stopLoadingScreen() {
+ guard let webView = webView else { return }
+ DispatchQueue.main.async {
+ webView.removeFromSuperview()
+ self.webView = nil
+ }
+ }
+ 
+ }
+ */
