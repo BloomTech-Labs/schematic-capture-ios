838d66dd56091660eeed1d89d189ff3c11db0160
Kerby Jean <kerby.jean@hotmail.fr>
2020-05-20 14:51:32 -0400

    Minor changes

---
+++ Schematic Capture/Schematic Capture/Views/ComponentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ComponentTableViewCell.swift
+//  Schematic Capture
+//
+//  Created by Kerby Jean on 5/19/20.
+//  Copyright © 2020 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class ComponentTableViewCell: UITableViewCell {
+
+    var indexLabel = UILabel()
+    var nameLabel = UILabel()
+//    var imageView
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
