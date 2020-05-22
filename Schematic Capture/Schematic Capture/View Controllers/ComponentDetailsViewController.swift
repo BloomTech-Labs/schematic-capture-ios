7a291a715c31704b548c171afcd5171d0eb68738
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-05-22 04:17:31 +0200

    Added Dictionary to ComponentDetailsView

--- Schematic Capture/Schematic Capture/View Controllers/ComponentDetailsViewController.swift
+++ Schematic Capture/Schematic Capture/View Controllers/ComponentDetailsViewController.swift
@@ -18,23 +18,28 @@ class ComponentDetailsViewController: UIViewController {
             headerView.setup(viewTypes: .componentDetails, value: [
                 (component?.componentDescription ?? ""), "Details", ""
             ])
-            details.append(component?.componentDescription ?? "")
-            details.append(component?.manufacturer ?? "")
-            details.append(component?.partNumber ?? "")
-            details.append(component?.rlCategory ?? "")
-            details.append(component?.rlNumber ?? "")
-            details.append(component?.stockCode ?? "")
-            details.append(component?.electricalAddress ?? "")
-            details.append(component?.referenceTag ?? "")
-            details.append(component?.settings ?? "")
-            details.append(component?.resources ?? "")
-            details.append(component?.cutSheet ?? "")
-            details.append(component?.storePartNumber ?? "")
-            details.append(component?.custom ?? "")
+            
+            let details: [String : String] = [
+                "Description:" : component?.componentDescription ?? "",
+                "Manufacturer:" : component?.manufacturer ?? "",
+                "Part #:" : component?.partNumber ?? "",
+                "RL Category:" : component?.rlCategory ?? "",
+                "RL Number:" : component?.rlNumber ?? "",
+                "Stock Code:" : component?.stockCode ?? "",
+                "Electrical Address:" : component?.electricalAddress ?? "",
+                "Reference Tag:" : component?.referenceTag ?? "",
+                "Settings:" : component?.settings ?? "",
+                "Resources:" : component?.resources ?? "",
+                "Cutsheet:" : component?.cutSheet ?? "",
+                "Store's Part #:" : component?.storePartNumber ?? "",
+                "Notes:" : component?.custom ?? "",
+            ]
+            self.details = details
         }
     }
     
-    var details = [String]()
+    var details = [String : String]()
+    
     
     // Labels
     private func setupViews() {
@@ -74,62 +79,32 @@ extension ComponentDetailsViewController: UITableViewDelegate, UITableViewDataSo
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let detail = details[indexPath.row]
+        var keys:[String] {
+            get{
+                return Array(details.keys)
+            }
+        }
+        let key = keys[indexPath.row]
+        let value = details[key]
         
         let cell = UITableViewCell()
-        //cell.detailTextLabel?.text = detail
-        
-        switch indexPath.row {
-            case 0:
-                cell.textLabel?.text = "Description:"
-                cell.detailTextLabel?.text = detail
-            case 1:
-                cell.textLabel?.text = "Manufacturer:"
-                cell.detailTextLabel?.text = detail
-            case 2:
-                cell.textLabel?.text = "Part #:"
-                cell.detailTextLabel?.text = detail
-            case 3:
-                cell.textLabel?.text = "RL Category:"
-                cell.detailTextLabel?.text = detail
-            case 4:
-                cell.textLabel?.text = "RL Number:"
-                cell.detailTextLabel?.text = detail
-            case 5:
-                cell.textLabel?.text = "Stock Code:"
-                cell.detailTextLabel?.text = detail
-            case 6:
-                cell.textLabel?.text = "Electrical Address:"
-                cell.detailTextLabel?.text = detail
-            case 7:
-                cell.textLabel?.text = "Reference Tag:"
-                cell.detailTextLabel?.text = detail
-            case 8:
-                cell.textLabel?.text = "Settings:"
-                cell.detailTextLabel?.text = detail
-            case 9:
-                cell.textLabel?.text = "Resources"
-                cell.detailTextLabel?.text = detail
-            case 10:
-                cell.textLabel?.text = "Cutsheet:"
-                cell.detailTextLabel?.text = detail
-            case 11:
-                cell.textLabel?.text = "Store's Part #:"
-                cell.detailTextLabel?.text = detail
-            case 12:
-                cell.textLabel?.text = "Notes:"
-                cell.detailTextLabel?.text = detail
-            default:
-                cell.detailTextLabel?.text = detail
-        }
+        cell.textLabel?.text = key
+        cell.detailTextLabel?.text = value
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let detail = details[indexPath.row]
+        var keys:[String] {
+            get{
+                return Array(details.keys)
+            }
+        }
+        let key = keys[indexPath.row]
+        let value = details[key]
+
         let editCoponentDetailViewController = EditCoponentDetailViewController()
-        editCoponentDetailViewController.value = detail
+        //editCoponentDetailViewController.value = detail
         navigationController?.pushViewController(editCoponentDetailViewController, animated: true)
     }
 }
