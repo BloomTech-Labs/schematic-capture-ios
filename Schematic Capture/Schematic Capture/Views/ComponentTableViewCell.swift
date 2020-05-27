27a4e20a75bea133267b75571fa6693468d7a252
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-05-28 00:39:47 +0200

    Added ClipsToBounds to ComponentsTableViewCell: componentsImageView

--- Schematic Capture/Schematic Capture/Views/ComponentTableViewCell.swift
+++ Schematic Capture/Schematic Capture/Views/ComponentTableViewCell.swift
@@ -27,6 +27,7 @@ class ComponentTableViewCell: UITableViewCell {
         nameLabel.text = "Electric motor"
         componentImageView.translatesAutoresizingMaskIntoConstraints = false
         componentImageView.contentMode = .scaleAspectFill
+        componentImageView.clipsToBounds = true
         componentImageView.image = UIImage(systemName: "camera")
         componentImageView.isUserInteractionEnabled = true 
         
@@ -41,8 +42,8 @@ class ComponentTableViewCell: UITableViewCell {
             nameLabel.heightAnchor.constraint(equalTo: heightAnchor),
             
             componentImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16.0),
-            componentImageView.heightAnchor.constraint(equalToConstant: 30.0),
-            componentImageView.widthAnchor.constraint(equalToConstant: 30.0),
+            componentImageView.heightAnchor.constraint(equalToConstant: 32.0),
+            componentImageView.widthAnchor.constraint(equalToConstant: 32.0),
             componentImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
             
             nameLabel.rightAnchor.constraint(equalTo: componentImageView.leftAnchor, constant: -8.0)
@@ -62,6 +63,8 @@ class ComponentTableViewCell: UITableViewCell {
         nameLabel.text = component.componentApplication
         guard let image = component.image else { return }
             print("IMAGE:", image)
+        
         self.componentImageView.sd_setImage(with: URL(string: image), completed: .none)
+
     }
 }
